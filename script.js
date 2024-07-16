717688256a19289f8d19ca6f90c4da890ed41cc2
XaRod18 <xander.rodriguez.2006@gmail.com>
2024-07-16 14:01:13 -0600

    Adds logic for hovering draw effect

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const container = document.querySelector('#container');
+
 const createSquares = () => {
     for (let i = 0; i < 16; i++) {
         let square = document.createElement("div");
         square.classList.add('child');
         container.appendChild(square);
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'red';
+        })
     }
 }
 
