06a05b66eb0b47d184de071ca778a910eff42245
XaRod18 <xander.rodriguez.2006@gmail.com>
2024-07-16 12:49:17 -0600

    Creates createSquares() to creat 16 divs

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('#container');
+const createSquares = () => {
+    for (let i = 0; i < 16; i++) {
+        let square = document.createElement("div");
+        container.appendChild(square);
+    }
+}
+
+createSquares();
