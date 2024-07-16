6a49f2c54382fe71f67f7e9c0daffbc7321d2725
XaRod18 <xander.rodriguez.2006@gmail.com>
2024-07-16 12:51:38 -0600

    Adds a class when creating divs

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('#container');
 const createSquares = () => {
     for (let i = 0; i < 16; i++) {
         let square = document.createElement("div");
+        square.classList.add('child');
         container.appendChild(square);
     }
 }
