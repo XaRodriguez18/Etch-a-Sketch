844936c16cb1e121d5700f5ed90b52dad5a347bf
XaRod18 <xander.rodriguez.2006@gmail.com>
2024-07-16 14:35:52 -0600

    Adds logic so user can determine sizing

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const container = document.querySelector('#container');
 
-const createSquares = () => {
-    for (let i = 0; i < 16; i++) {
+const createSquares = (rows) => {
+    for (let i = 0; i < (rows * rows); i++) {
         let square = document.createElement("div");
         square.classList.add('child');
+        squareWidth = 100 / rows;
+        square.style.width = `${squareWidth}%`
         container.appendChild(square);
         square.addEventListener('mouseover', () => {
             square.style.backgroundColor = 'red';
@@ -11,4 +13,17 @@ const createSquares = () => {
     }
 }
 
-createSquares();
+createSquares(16);
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', () => {
+    rows = prompt('Enter a value between 0 and 100');
+    container.innerHTML = '';
+    if (rows > 0 && rows < 101) {
+        createSquares(rows);
+    } else {
+        alert('Only input values between 0 and 100');
+        createSquares(16);
+    }
+})
